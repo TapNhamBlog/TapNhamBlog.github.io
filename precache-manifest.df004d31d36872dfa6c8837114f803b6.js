1188ed086ca95dee471e0004096d91ad549ba19a
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-01-02 16:19:08 +0700

    Updates

---
+++ precache-manifest.df004d31d36872dfa6c8837114f803b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a65729d42699e3c6189654062af726c",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/index.html"
+  },
+  {
+    "revision": "cf4bef75929599119ad9",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a687bff2e2dd263e556a",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/static/js/2.4ee43297.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/static/js/2.4ee43297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf4bef75929599119ad9",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/static/js/main.b3549193.chunk.js"
+  },
+  {
+    "revision": "49eecc5cdd08ce0fa956",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/static/js/runtime-main.a2cf4db6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TapNhamBlog/TapNhamBlog.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
